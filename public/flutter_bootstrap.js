d2c7e6a750397d55ef1ce852f3e768018527d451
Seungho Jang <hoosong0235@gmail.com>
2024-06-11 03:30:14 +0000

    v1.2.0

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2416820594"
+    serviceWorkerVersion: "1353399600"
   }
 });
